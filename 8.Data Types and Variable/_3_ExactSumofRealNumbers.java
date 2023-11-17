cba7709110242db82b2d482e7d55dbb2f9be101d
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2023-11-17 15:48:32 +0000

    using BigDecimal in exercise

---
+++ 8.Data Types and Variable/_3_ExactSumofRealNumbers.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class _3_ExactSumofRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        BigDecimal result = new BigDecimal("0");
+        for (int i = 0; i < n ; i++) {
+            BigDecimal number = new BigDecimal(scanner.nextLine());
+            result = result.add(number);
+        }
+        System.out.println(result);
+    }
+}
