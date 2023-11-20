d8ab1d19cc620b1eedfc2628d7afcdf65fe2464a
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2023-11-20 20:31:07 +0000

    adding exercise

---
+++ 9.Data Types and Variables - Exercise/_2_SumDigit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class _2_SumDigit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        while (number > 0){
+            int lastDigit = number % 10;
+            sum += lastDigit;
+            number /= 10;
+        }
+        System.out.println(sum);
+    }
+}
