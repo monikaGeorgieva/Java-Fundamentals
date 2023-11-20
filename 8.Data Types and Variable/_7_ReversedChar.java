d7edf28bcf376acc1cad0e594eaa0e0f3d2a78d3
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2023-11-20 19:43:53 +0000

    adding new exercises

---
+++ 8.Data Types and Variable/_7_ReversedChar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class _7_ReversedChar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String char1 = scanner.nextLine();
+        String char2 = scanner.nextLine();
+        String char3 = scanner.nextLine();
+
+        System.out.printf("%s %s %s",char3,char2,char1);
+
+    }
+}
+//A
+//B
+//C
+//output:
+//CBA
---
+++ 8.Data Types and Variable/_8_LowerOrUpper.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class _8_LowerOrUpper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char c = scanner.nextLine().charAt(0);
+
+        if (c >= 65 && c <= 90) {
+            System.out.println("upper-case");
+        } else if (c >= 97 && c <= 122) {
+            System.out.println("lower-case");
+        }
+    }
+
+}
