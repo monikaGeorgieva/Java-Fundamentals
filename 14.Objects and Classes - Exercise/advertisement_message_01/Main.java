0983b66ffa012e98f3b0d9260e1cbfa4386355d6
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2024-02-27 19:39:38 +0000

    Update Main.java

--- 14.Objects and Classes - Exercise/advertisement_message_01/Main.java
+++ 14.Objects and Classes - Exercise/advertisement_message_01/Main.java
@@ -1,36 +1,21 @@
+
 package advertisement_message_01;
 
-import java.util.Random;
+import java.util.Scanner;
 
-public class Message {
+public class Main {
 
-    private Random random = new Random();
+    public static void main(String[] args) {
 
-    private String[] phrases = {
-            "Excellent product.",
-            "Such a great product.",
-            "I always use that product.",
-            "Best product of its category.",
-            "Exceptional product.",
-            "I can’t live without this product."
-    };
-    private String[] events = {
-            "Now I feel good.",
-            "I have succeeded with this product.",
-            "Makes miracles. I am happy of the results!",
-            "I cannot believe but now I feel awesome.",
-            "Try it yourself, I am very satisfied.",
-            "I feel great!"
-    };
-    private String[] author = {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
-    private String[] town = {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
+        Scanner scanner = new Scanner(System.in);
 
-    public String randomMessage() {
+        int n = Integer.parseInt(scanner.nextLine());
 
-        return String.format("%s %s %s - %s",
-                phrases[random.nextInt(phrases.length)],
-                events[random.nextInt(events.length)],
-                author[random.nextInt(author.length)],
-                town[random.nextInt(town.length)]);
+        Message message = new Message();
+
+        for (int i = 0; i < n; i++) {
+            String randomMessage = message.randomMessage();
+            System.out.println(randomMessage);
+        }
     }
 }
