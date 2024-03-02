d3419cb9b8ff0c577167eb14545beb80da8a36bf
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2024-03-02 10:42:27 +0000

    Create Substring03.java

---
+++ 16.Text Processing/Substring03.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Substring03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String toRemove = scanner.nextLine();
+        String text = scanner.nextLine();
+
+        int index = text.indexOf(toRemove);
+        while (index != -1){
+            text = text.replace(toRemove,"");
+
+            index = text.indexOf(toRemove);
+        }
+        System.out.println(text);
+
+    }
+}
+//ice
+//kicegiciceeb
+//output
+//kgb
