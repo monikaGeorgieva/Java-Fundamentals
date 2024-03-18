437dcda3f46ea32812d9e8522f86b6c112bf75ac
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2024-03-18 20:50:36 +0000

    Update _1_MatchFullName.java

--- 18.Regular Expresion Lab/_1_MatchFullName.java
+++ 18.Regular Expresion Lab/_1_MatchFullName.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class _1_MatchFullName {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      String text = scanner.nextLine();
+      String regex = "\\b([A-Z][a-z]+) ([A-Z][a-z]+)";
+        Pattern pattern = Pattern.compile(regex); //създаваме шаблон
+        Matcher matcher = pattern.matcher(text);
+
+      //  matcher.find() --> следващото попадение true/falce
+        while (matcher.find()){
+            System.out.print(matcher.group() + " ");
+        }
+    }
+}
+//peter georgiev, peter Georgiev, Peter GeoRgiev, PEter GEorgiev, Peter Georgiev, Anna
+//Petrova
