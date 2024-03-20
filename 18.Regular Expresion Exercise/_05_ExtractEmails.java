bccbb985ebca5de9179381d3ed8c9eb90b70dd92
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2024-03-20 13:23:54 +0000

    Create _05_ExtractEmails.java

---
+++ 18.Regular Expresion Exercise/_05_ExtractEmails.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class _05_ExtractEmails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String regex = "([A-Za-z0-9]+[\\.\\-\\_]?[A-Za-z]+)@[A-Za-z]+\\.?[A-Za-z]+\\.?[A-Za-z]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
+//Just send email to s.miller@mit.edu and j.hopking@york.ac.uk
+//for more information.
+
+//output:
+//s.miller@mit.edu
+//j.hopking@york.ac.uk
