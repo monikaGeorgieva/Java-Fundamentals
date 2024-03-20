1a3423e34f15daed1812e8639f40c088b5d6fef6
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2024-03-20 13:34:37 +0000

    Update _05_ExtractEmails.java

--- 18.Regular Expresion Exercise/_05_ExtractEmails.java
+++ 18.Regular Expresion Exercise/_05_ExtractEmails.java
@@ -6,7 +6,7 @@ public class _05_ExtractEmails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        String regex = "([A-Za-z0-9]+[\\.\\-\\_]?[A-Za-z]+)@[A-Za-z]+\\.?[A-Za-z]+\\.?[A-Za-z]+";
+        String regex = "[A-Za-z0-9]+[\.\-\_]?[A-Za-z0-9]+@[A-Za-z]+\-?[A-Za-z]+(\.[A-Za-z]+\-?[A-Za-z]+)+";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()){
